d09ab7a9d6f1d2f5af79ad4b977d7c52c42c1e4a
Avenash Krishnakumar <avenash555@gmail.com>
2015-09-28 18:45:10 +0530

    Task tag display

--- ead-hibernate-app/web/scripts/app.js
+++ ead-hibernate-app/web/scripts/app.js
@@ -9,16 +9,15 @@ $(document).ready(function() {
     var counter = 2;
 
     $("#addButton").click(function() {
-        
+
         var newTextBoxDiv = $(document.createElement('div'))
                 .attr("id", 'TextBoxDiv' + counter);
 
-        newTextBoxDiv.after().html(
+        newTextBoxDiv.after().html('Task ' + counter + ' - ' +
                 '<input type="text" name="task' + counter +
                 '" id="textbox' + counter + '" value="" style="width : 200px; " required>');
 
         newTextBoxDiv.appendTo("#TextBoxesGroup");
-
 
         counter++;
     });
@@ -43,4 +42,27 @@ $(document).ready(function() {
         }
         alert(msg);
     });
+    ///
+//
+//    function loadXMLDoc() {
+//        var xmlhttp = new XMLHttpRequest();
+//        xmlhttp.onreadystatechange = function() {
+//            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+//                myFunction(xmlhttp.responseXML);
+//            }
+//        }
+//        xmlhttp.open("GET", "app.xml", true);
+//        xmlhttp.send();
+//    }
+//    function myFunction(xmlDoc) {
+//        var i;
+//        var table = "<tr><th>Artist</th><th>Title</th></tr>";
+//        var x = xmlDoc.getElementsByTagName("app-name");
+//        for (i = 0; i < x.length; i++) {
+//            $('#app-name').text(x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue);
+//        }
+//        document.getElementById("demo").innerHTML = table;
+//    }
+
+    ///
 });
